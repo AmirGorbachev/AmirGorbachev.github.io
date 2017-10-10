013445206fabc5af4694571c5cd29151063c48c9
AmirGorbachev <amir.gorbachyow@yandex.ru>
2017-10-11 04:14:06 +0600

    DeleteForm
    DeleteForm

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,22 +6,5 @@ $(document).ready(function(){
 	        $('body,html').animate({scrollTop: top}, 750);
 	    });
 	});
-<<<<<<< HEAD
-$(document).ready(function(){
-    $("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
-            var form_data = $(this).serialize(); //собераем все данные из формы
-            $.ajax({
-            type: "POST", //Метод отправки
-            url: "send.php", //путь до php фаила отправителя
-            data: form_data,
-            success: function() {
-                   //код в этом блоке выполняется при успешной отправке сообщения
-                   alert("Ваше сообщение отпрвлено!");
-            });
-    });
-});
-=======
-
->>>>>>> parent of 2877b83... JSforMail
 
 
