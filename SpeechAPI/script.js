badfb73388e87383d2dfd4c97c412075ef68226a
Amir Gorebachev <32037802+AmirGorbachev@users.noreply.github.com>
2018-08-24 00:40:16 +0600

    Upd Ottergram

--- SpeechAPI/script.js
+++ SpeechAPI/script.js
@@ -8,3 +8,4 @@ function talk () {
 function stop () {
   speechSynthesis.cancel();
 }
+
--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,14 +1,16 @@
 'use strict';
 
-var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
-var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
-var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
-	var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+	let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
 	detailImage.src = imageUrl;
 
-	var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+	let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
  	detailTitle.textContent = titleText;
 }
 
@@ -28,18 +30,38 @@ function addThumbClickHandler(thumb){
 	thumb.addEventListener('click', function (event) {
 	event.preventDefault();
 	setDetailsFromThumb(thumb);
+	showDetails();
  });
 }
 
 function getThumbnailsArray() {
-	var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
-	var thumbnailArray = [].slice.call(thumbnails);
+	let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+	let thumbnailArray = [].slice.call(thumbnails);
 	return thumbnailArray;
 }
 
+function hideDetails() {
+	document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+	document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
+function addKeyPressHandle() {
+	document.body.addEventListener ('keyup', function(event) {
+		event.preventDefault();
+		console.log(event.keyCode);
+		if (event.keyCode === ESC_KEY) {
+ 			hideDetails();
+ 		}
+	});
+}
+
 function initializeEvents() {
-	var thumbnails = getThumbnailsArray();
+	let thumbnails = getThumbnailsArray();
 	thumbnails.forEach(addThumbClickHandler);
+	addKeyPressHandle();
 }
 
 initializeEvents();
